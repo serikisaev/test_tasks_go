cd7ca0ac8a206450532b554a6991f8cddb6306b8
serik <serikisaev87@gmail.com>
2020-05-25 14:27:08 +0500

    add file

--- 04_addition_of_numbers.py
+++ 04_addition_of_numbers.py
@@ -1,6 +1,2 @@
-def addition_of_numbers(data):
-    sub_list = data.split()
-    return int(sub_list[0]) + int(sub_list[1])
-
-
-addition_of_numbers('1 2')
+sub_list = input().split()
+print(int(sub_list[0]) + int(sub_list[1]))
