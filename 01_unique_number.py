55bc11370deb409b02fdf60982f701f0944595fc
serik <serikisaev87@gmail.com>
2020-05-25 13:20:25 +0500

    Create

---
+++ 01_unique_number.py
@@ -0,0 +1,19 @@
+def unique_number(file):
+    sub_list = []
+    sub_list_2 = []
+
+    with open(file, 'r') as sub_file:
+        for line in sub_file:
+            sub_list.append(int(line))
+
+    for i in sub_list:
+        if sub_list.count(i) == 1:
+            sub_list_2.append(i)
+
+    with open('input-201.a.txt', 'w') as sub_file_2:
+        for i in sub_list_2:
+            print(i, file=sub_file_2)
+
+
+if __name__ == "__main__":
+    unique_number('input-201.txt')
